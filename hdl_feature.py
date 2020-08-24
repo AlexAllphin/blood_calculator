b1c2eb68f1ca0b3ca2674724589dc0c14f993dfb
Alex Allphin <alex.allphin@duke.edu>
2020-08-24 13:13:52 -0400

    Created Driving function to call other functions

--- hdl_feature.py
+++ hdl_feature.py
@@ -1,6 +1,9 @@
+# Input takes in an HDL value
 def input_function():
     hdl = input("Enter HDL: ")
     return int(hdl)
+
+# Check HDl analyzes the HDL value to return useful information
 
 
 def check_HDL(hdl):
@@ -10,3 +13,10 @@ def check_HDL(hdl):
         return "Low"
     else:
         return "Borderline Low"
+
+# Driver Function calls input_function and check_HDL
+
+
+def Driver():
+    x = input_function()
+    check_HDL(x)
