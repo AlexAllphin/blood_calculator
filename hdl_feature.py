d6fc3a8170f30eb0807d1a4197a530ee161f999a
Alex Allphin <alex.allphin@duke.edu>
2020-08-24 13:08:55 -0400

    Created function to check hdl value

--- hdl_feature.py
+++ hdl_feature.py
@@ -1,3 +1,12 @@
 def input_function():
     hdl = input("Enter HDL: ")
     return int(hdl)
+
+
+def check_HDL(hdl):
+    if hdl >= 60:
+        return "Normal"
+    elif hdl < 40:
+        return "Low"
+    else:
+        return "Borderline Low"
